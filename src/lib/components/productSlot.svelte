f8cab06a0c397207e6ff4cde7db3fc563385d4e5
Guitar Hero <guitarhero@Guitars-iMac.fritz.box>
2025-12-02 09:52:19 +0100

    underline in app.css entfernt. und slotMinutes korigirt

--- src/lib/components/productSlot.svelte
+++ src/lib/components/productSlot.svelte
@@ -3,14 +3,15 @@
 </script>
 
 <a href="/veranstaltungen/{product.id}" class="filterSlots">
-    <div class="font-bold text-lg border-b border-gray-600 pb-2">
+    <div>
         Product ID: {product.id}
     </div>
-    <div class="text-sm">
+    <div>
+        <!-- kommen aus den bookings daher werden startDate nicht angezeigt -->
         <p><strong>Start Date:</strong> {product.startDate || "N/A"}</p>
-        <p><strong>Slot Minutes:</strong> {product.slotMinuts || "N/A"}</p>
+        <p><strong>Slot Minutes:</strong> {product.slotMinutes || "N/A"}</p>
     </div>
-    <div class="text-sm mt-2">
+    <div>
         <p>
             <strong>Description:</strong>
             {product.translations && product.translations.length > 0
